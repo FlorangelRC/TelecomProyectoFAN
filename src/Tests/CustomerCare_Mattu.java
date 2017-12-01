842f4c320c4dca5d89573e53e929099ede0f8b84
Unknown <matias.maretto@gmail.com>
2017-12-01 10:57:37 -0300

    Mattu

--- src/Tests/CustomerCare_Mattu.java
+++ src/Tests/CustomerCare_Mattu.java
@@ -4,11 +4,12 @@ import static org.testng.Assert.assertTrue;
 
 import java.util.List;
 
-import org.junit.Assert;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
