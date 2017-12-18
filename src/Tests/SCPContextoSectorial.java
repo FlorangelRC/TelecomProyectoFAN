770250fc7eaa53273c99f52b936236a3f49ee1eb
Nico <nicolas.fazzano@gmail.com>
2017-12-18 10:54:12 -0300

    Actualizacion SCP

--- src/Tests/SCPContextoSectorial.java
+++ src/Tests/SCPContextoSectorial.java
@@ -15,6 +15,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import Pages.BasePage;
+import Pages.CustomerCare;
 import Pages.SCP;
 import Pages.setConexion;
 
@@ -618,7 +619,9 @@ private WebDriver driver;
 		List <WebElement> a = driver.findElements(By.className("resetHito"));
 		a.get(0).sendKeys(c);
 		a.get(1).click();
-		driver.findElement(By.xpath("//*[text() = 'Otro']")).click();
+		CustomerCare page = new CustomerCare(driver);
+		page.setSimpleDropdown(a.get(1), "Otro");
+		try {Thread.sleep(2000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		driver.findElement(By.className("dateFormat")).click();
 		driver.findElement(By.cssSelector(".btn.btn-primary")).click();
 		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
