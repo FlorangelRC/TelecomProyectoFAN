73f422198938f63f7e5e68f36d7f079ba9c73518
Leo <leonardo.jerez@xappia.com>
2017-12-29 10:31:14 -0300

    SCP entrar a Oportunidad

--- src/Tests/SCPParquedeServicios.java
+++ src/Tests/SCPParquedeServicios.java
@@ -73,13 +73,13 @@ public class SCPParquedeServicios extends TestBase{
 		}
 	}
 	
-	@AfterMethod(groups= "SCP")
+//	@AfterMethod(groups= "SCP")
 	public void after(){
 		((JavascriptExecutor)driver).executeScript("window.scrollTo(0,"+driver.findElement(By.id("home_Tab")).getLocation().y+")");
 		driver.findElement(By.id("home_Tab")).click();
 	}
 	
-   @AfterClass(groups= "SCP")
+  // @AfterClass(groups= "SCP")
 	public void tearDown() {
 		driver.quit();
 		sleep(4000);
@@ -344,4 +344,16 @@ public class SCPParquedeServicios extends TestBase{
    pcp.moveToElementOnAccAndClick("segundoTitulo", 2);
       pcp.validarcomentarioajeno(fecha.toString());
  }	
+	@Test(groups = "SCP") 
+	public void TS112645_Estructura_de_las_oportunidades_Bloques_Estado_de_Proyecto_DELTA_Oportunidad(){
+		SCP page = new SCP(driver);
+		page.clickOnTabByName("cuentas");
+		page.clickOnFirstAccRe();
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		page.firstoportunidad();
+		
+		
+	}
+	
+	
 }
