18f634d6d44f724528f7554bf254a83ac79e1ca6
Leo <leonardo.jerez@xappia.com>
2018-01-04 09:46:44 -0300

    assads

--- src/Pages/SCP.java
+++ src/Pages/SCP.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -557,11 +558,143 @@ Assert.assertTrue(acc&&nmbre&&pntf&&pntd);
 	public void validarinfoventas(){
 		List<WebElement> secc = driver.findElements(By.cssSelector(".pbSubsection"));
 		WebElement element = secc.get(1);
+		 ArrayList<String> txt1 = new ArrayList<String>();
+		 ArrayList<String> txt2 = new ArrayList<String>();
+		 txt2.add("Tipo");
+		 txt2.add("Razón perdida");
+		 txt2.add("Estado de la oportunidad");
+		 txt2.add("Creado por");
+		 txt2.add("Propietario de oportunidad");
+		 txt2.add("Última modificación por");
+		 txt2.add("Descripci\u00f3n");
+
 		((JavascriptExecutor)driver).executeScript("window.scrollTo(0,"+element.getLocation().y+")");
 		List<WebElement> vnts = element.findElements(By.cssSelector(".labelCol"));
 		for(WebElement e: vnts){
-		System.out.println(e.getText());
-		}
+			System.out.println(e.getText());
+			 txt1.add(e.getText());
+		 }
+		 Assert.assertTrue(txt1.containsAll(txt2));
 	}
 
+		//Entra desde una cuenta > oportunidad > productos
+	public void IngresarAlProductos(String producto){
+		WebElement element = driver.findElement(By.cssSelector(".listRelatedObject.opportunityLineItemBlock"));
+		((JavascriptExecutor)driver).executeScript("window.scrollTo(0,"+element.getLocation().y+")");
+		List<WebElement> op = element.findElements((By.cssSelector(".dataCell")));
+		for(WebElement e : op){
+			System.out.println(e.getText());
+			if(e.getText().equals(producto)){
+				e.findElement(By.tagName("a")).click();
+				break;}}
+	}
+	
+	//Entra desde una cuenta > oportunidad > productos
+	public void ModificarProducto(String ModificarEliminar, String campo , String dato, String GuardarCancelar){
+		List<WebElement> btns = driver.findElements(By.cssSelector(".btn"));
+		for(WebElement e: btns){
+			if(e.getAttribute("value").toLowerCase().equals(ModificarEliminar.toLowerCase())){
+				e.click();
+				if(ModificarEliminar.toLowerCase().equals("eliminar")){
+					 Alert alert = driver.switchTo().alert();
+					   alert.accept();}
+				break;}}
+		Actions action = new Actions(driver);   
+		switch(campo.toLowerCase()){
+		case "precio":
+			driver.findElement(By.id("UnitPrice_ileinner")).click();
+			action.moveToElement(driver.findElement(By.id("UnitPrice_ileinner"))).doubleClick().perform();
+			driver.findElement(By.id("UnitPrice")).clear();
+			driver.findElement(By.id("UnitPrice")).sendKeys(dato);
+				break;
+		case "cantidad":
+			driver.findElement(By.id("Quantity")).clear();
+			driver.findElement(By.id("Quantity")).sendKeys(dato);
+				break;
+		case "plazo":
+		
+			driver.findElement(By.id("00N3F000000HaZN")).clear();
+			driver.findElement(By.id("00N3F000000HaZN")).sendKeys(dato);
+				break;
+		case "cargo unico":			  
+		
+			driver.findElement(By.id("00N3F000000HaZH")).clear();
+			driver.findElement(By.id("00N3F000000HaZH")).sendKeys(dato);
+				break;
+		case "moneda":
+	
+			setSimpleDropdown(driver.findElement(By.id("00N3F000000HaZL")), dato);
+				break;
+		case "descripcion":			  
+		
+			driver.findElement(By.id("Description_ileinneredit")).clear();
+			driver.findElement(By.id("Description_ileinneredit")).sendKeys(dato);
+				break;
+		case "comentarios":			  
+		
+			driver.findElement(By.id("00N3F000000HaZJ")).clear();
+			driver.findElement(By.id("00N3F000000HaZJ")).sendKeys(dato);
+				break;}
+		
+		List<WebElement> btns2 = driver.findElements(By.cssSelector(".btn"));
+		for(WebElement e: btns2){
+			if(e.getAttribute("value").toLowerCase().equals(GuardarCancelar.toLowerCase())){
+				e.click();
+			break;}
+			}
+	}
+	
+	public void VerificarCampoModificado(String campo , String dato){
+		switch(campo.toLowerCase()){
+		case "precio":
+		Assert.assertTrue(driver.findElement(By.id("UnitPrice_ileinner")).getText().contains(dato));
+				break;
+		case "cantidad":
+			Assert.assertTrue(driver.findElement(By.id("Quantity_ileinner")).getText().contains(dato));
+				break;
+		case "plazo":
+			Assert.assertTrue(driver.findElement(By.id("00N3F000000HaZN_ileinner")).getText().contains(dato));
+				break;
+		case "cargo unico":		
+			Assert.assertTrue(driver.findElement(By.id("00N3F000000HaZH_ileinner")).getText().contains(dato));
+				break;
+		case "moneda":
+			Assert.assertTrue(driver.findElement(By.id("00N3F000000HaZL_ileinner")).getText().contains(dato));
+				break;
+		case "descripcion":			  
+			Assert.assertTrue(	driver.findElement(By.id("Description_ileinner")).getText().contains(dato));
+				break;
+		case "comentarios":			  
+			Assert.assertTrue(driver.findElement(By.id("00N3F000000HaZJ_ileinner")).getText().contains(dato));
+				break;}
+	}
+	
+	public String CargosTotalesPorMes(){
+		TestBase TB = new TestBase();
+		TB.waitFor(driver, By.id("00N3F000000HaZI_ileinner"));
+		String a;
+		a = driver.findElement(By.id("00N3F000000HaZI_ileinner")).getText();
+		return a;
+	}
+	
+	public void ValidarMontoContrato(){
+		String CUV0 = driver.findElement(By.id("00N3F000000HaZH_ileinner")).getText().substring(1).replaceAll(",", ".");
+		double CUV= Integer.parseInt(CUV0); 
+		String cant0 = driver.findElement(By.id("Quantity_ileinner")).getText().substring(1).replaceAll(",", ".");
+		int cant = Integer.parseInt(cant0);
+		int plazo = Integer.parseInt(driver.findElement(By.id("00N3F000000HaZN_ileinner")).getText()); 
+		String abono0 =driver.findElement(By.id("UnitPrice_ileinner")).getText().substring(1).replaceAll(",", ".");
+		int abono = Integer.parseInt(abono0); 
+		String TC = driver.findElement(By.id("00N3F000000HaZK_ileinner")).getText().substring(1).replaceAll(",","." );
+		double TotalContrato = Integer.parseInt(TC); 
+		double FORMULA = CUV+(cant*plazo*abono);
+		String contrato = Double.toString(TotalContrato);
+		String resultado = Double.toString(FORMULA);
+		System.out.println(resultado);
+		System.out.println(contrato);
+
+		Assert.assertEquals(contrato, resultado);
+		
+
+	}
 }
--- src/Tests/SCPParquedeServicios.java
+++ src/Tests/SCPParquedeServicios.java
@@ -34,6 +34,7 @@ public class SCPParquedeServicios extends TestBase{
 	String categoria = "Servicio automatizado";
 	String servicio = "Prueba automatizada";
 	String color = "Rojo";
+	
 	@BeforeClass(groups= "SCP")
 	public void init() throws Exception
 	{
@@ -382,4 +383,68 @@ public void TS112648_Estructura_de_las_oportunidades_Bloques_Informacion_Adicion
 		page.validarinfoventas();
 	}
 	
+	@Test(groups = "SCP") 
+	public void TS112652_Estructura_de_las_oportunidades_Bloques_Productos_de_la_oportunidad_Campos_no_editables(){
+		SCP page = new SCP(driver);
+		page.clickOnTabByName("cuentas");
+		page.clickEnCuentaPorNombre("AIR S.R.L");
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		page.elegiroportunidad("ALta suc. Jujuy");
+		page.IngresarAlProductos("HYPERWAY HEAD QUARTER");
+		page.ModificarProducto("modificar","cantidad", "5,00", "guardar");
+		page.VerificarCampoModificado("cantidad", "5,00");
+	}
+	
+	@Test(groups = "SCP") 
+	public void TS112658_Estructura_de_las_oportunidades_Bloques_Productos_de_la_oportunidad_Cargo_Unica_Vez_Modificar(){
+		SCP page = new SCP(driver);
+		page.clickOnTabByName("cuentas");
+		page.clickEnCuentaPorNombre("AIR S.R.L");
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		page.elegiroportunidad("ALta suc. Jujuy");
+		page.IngresarAlProductos("HYPERWAY HEAD QUARTER");
+		page.ModificarProducto("modificar","cargo unico", "600,00", "guardar");
+		page.VerificarCampoModificado("cargo unico", "600,00");
+	}
+	
+	@Test(groups = "SCP") 
+	public void TS1112659_Estructura_de_las_oportunidades_Bloques_Productos_de_la_oportunidad_Cargos_Totales_por_Mes(){
+		SCP page = new SCP(driver);
+		page.clickOnTabByName("cuentas");
+		page.clickEnCuentaPorNombre("AIR S.R.L");
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		page.elegiroportunidad("ALta suc. Jujuy");
+		page.IngresarAlProductos("HYPERWAY HEAD QUARTER");
+		page.ModificarProducto("modificar","cantidad", "8", "guardar");
+		String a = page.CargosTotalesPorMes();
+		page.ModificarProducto("modificar","cantidad", "5", "guardar");
+		String b = page.CargosTotalesPorMes();
+		Assert.assertFalse(a.equals(b));
+	}
+	
+	@Test(groups = "SCP") 
+	public void TS112661_Estructura_de_las_oportunidades_Bloques_Productos_de_la_oportunidad_Plazo_Modificar(){
+		SCP page = new SCP(driver);
+		page.clickOnTabByName("cuentas");
+		page.clickEnCuentaPorNombre("AIR S.R.L");
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		page.elegiroportunidad("ALta suc. Jujuy");
+		page.IngresarAlProductos("HYPERWAY HEAD QUARTER");
+		page.ModificarProducto("modificar","plazo", "50", "guardar");
+		page.VerificarCampoModificado("plazo", "50");
+	}
+	
+	@Test(groups = "SCP") 
+	public void TS1112662_Estructura_de_las_oportunidades_Bloques_Productos_de_la_oportunidad_Precio_total_contrato(){
+		SCP page = new SCP(driver);
+		page.clickOnTabByName("cuentas");
+		page.clickEnCuentaPorNombre("AIR S.R.L");
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		page.elegiroportunidad("ALta suc. Jujuy");
+		page.IngresarAlProductos("HYPERWAY HEAD QUARTER");
+		page.ModificarProducto("modificar","cargo unico", "900,00", "guardar");
+		page.VerificarCampoModificado("cargo unico", "900,00");
+		page.ValidarMontoContrato();
+		
+	}
 }
