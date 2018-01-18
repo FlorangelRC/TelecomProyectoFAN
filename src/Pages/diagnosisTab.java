ac1d93ac51ce78680e0d56e279a26039b7276dd3
FlorangelRC <florangel.r.c@gmail.com>
2018-01-18 17:12:02 -0300

    nuevos casos tech

--- src/Pages/diagnosisTab.java
+++ src/Pages/diagnosisTab.java
@@ -3,11 +3,13 @@ package Pages;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class diagnosisTab extends BasePage{
 	
@@ -60,5 +62,37 @@ public void clickOnExeccute() {
 	     }
 	}
 	
+	public void irADiagnostico(WebDriver driver, String opcion, WebElement card) {
+		BasePage cambioFrameByID=new BasePage();
+		 card.findElement(By.className("card-info")).findElement(By.className("details")).click();
+	     try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+	     driver.switchTo().defaultContent();
+	     driver.switchTo().frame(cambioFrameByID.getFrameForElement(driver, By.cssSelector(".slds-small-size--3-of-12.slds-medium-size--3-of-12.slds-large-size--3-of-12.flyout-actions")));
+	     ((JavascriptExecutor)driver).executeScript("window.scrollTo(0,"+driver.findElement(By.cssSelector(".console-flyout.active.flyout")).getLocation().y+")");
+	     try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();} 
+	     List<WebElement> gestiones = driver.findElement(By.className("community-flyout-actions-card")).findElements(By.tagName("a"));
+	     for (WebElement UnaG : gestiones) {
+	    	 System.out.println("opc: "+UnaG.findElement(By.tagName("span")).getText());
+		    	if (UnaG.findElement(By.tagName("span")).getText().toLowerCase().contains(opcion)) {
+		    		UnaG.click();
+		    		break;
+		      }
+		     }
+	    
+	}
+	
+	public boolean seleccionarMotivoPorSelect(WebDriver driver, String motivo) {
+		Select listSelect = new Select(driver.findElement(By.id("Motive")));
+		listSelect.selectByVisibleText(motivo);
+		return(listSelect.getFirstSelectedOption().getText().equals(motivo));
+	}
+	
+	public void funcionoConfiguracion(WebDriver driver, String resp) {
+		if (resp.toLowerCase().equals("si"))
+			driver.findElement(By.id("SolutionValidationRadio|0")).findElement(By.cssSelector(".slds-radio--faux.ng-scope")).click();
+		else
+			driver.findElement(By.id("SolutionValidationRadio|0")).findElements(By.cssSelector(".slds-radio--faux.ng-scope")).get(1).click();
+		
+	}
 	
 }
