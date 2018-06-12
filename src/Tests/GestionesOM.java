a737013bdbad2c20d283e60a60e78aa04c765b60
Alan <alan.rodriguez@xappia.com>
2018-06-12 14:51:47 -0300

    ASGDAsgfd

--- src/Tests/GestionesOM.java
+++ src/Tests/GestionesOM.java
@@ -7,9 +7,11 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
@@ -125,5 +127,64 @@ public class GestionesOM extends TestBase {
 		pageOm.cambiarVentanaNavegador(0);
 		sleep(12000);
 	}
+	
+	//=========================================================CAMBIO DE NUMERO==================================================================================
+	
+		@Test (groups = "OM")
+		public void TS_OM_Cambio_de_Numero(){
+			Date date = new Date();
+			OM om = new OM(driver);
+		//Mientras, seleccion de vista
+			Select allOrder=new Select(driver.findElement(By.id("fcf")));
+			allOrder.selectByVisibleText("AlanOM");
+			sleep(1000);
+			try {driver.findElement(By.name("go")).click();}catch(org.openqa.selenium.NoSuchElementException e) {}
+			sleep(3000);
+		//Selecciona la primera cuenta de la lista en la vista seleccionada
+			WebElement primeraCuenta=driver.findElement(By.cssSelector(".x-grid3-col.x-grid3-cell.x-grid3-td-SALES_ACCOUNT_NAME"));
+			primeraCuenta.findElement(By.tagName("div")).findElement(By.tagName("a")).click();
+			sleep(8000);
+		//Seleccion del ultimo Asset
+			om.irAChangeToOrder();	
+			sleep(8000);
+		//Ingreso de fecha avanzada
+			Accounts accountPage = new Accounts(driver);
+			/*DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+			driver.findElement(By.id("RequestDate")).sendKeys(dateFormat.format(om.fechaAvanzada()));*/
+			driver.findElement(By.id("RequestDate")).sendKeys("08-10-2018");
+			driver.findElement(By.cssSelector(".form-control.btn.btn-primary.ng-binding")).click();
+			sleep(15000);
+		//SIM
+			driver.findElement(By.cssSelector(".slds-button.cpq-item-has-children")).click();
+			sleep(3000);
+			driver.switchTo().defaultContent();
+			driver.findElement(By.xpath(".//*[@id='tab-default-1']/div[1]/ng-include/div/div/div/div[4]/div[2]/div/ng-include/div/div[2]/ng-include/div/div[1]/div/div[2]/div[11]")).click();
+			sleep(3000);
+			driver.findElement(By.xpath(".//*[@id='tab-default-1']/div[1]/ng-include/div/div/div/div[4]/div[2]/div/ng-include/div/div[2]/ng-include/div/div[1]/div/div[2]/div[11]/div[2]/div/ul/li[3]/a")).click();
+			sleep(5000);
+			((JavascriptExecutor)driver).executeScript("window.scrollTo(0,"+driver.findElement(By.className("slds-section")).getLocation().y+" )");
+			WebElement msi = driver.findElement(By.xpath("//*[@id='js-cpq-product-cart-config-form']/div[1]/div/form/div[17]/div[1]/input"));
+			Random r = new Random();
+			msi.clear();
+			msi.sendKeys("11" + r.nextInt(200000000) );
+			msi.submit();
+			sleep(30000);
+			driver.findElement(By.id("-import-btn")).click();
+			sleep(5000);
+		//Gestion
+			om.agregarGestion("Cambio de n\u00famero");
+			driver.findElements(By.id("topButtonRow")).get(0);
+			sleep(7000);
+			driver.findElement(By.name("ta_submit_order")).click();
+			sleep(35000);
+			om.cambiarVentanaNavegador(1);
+			sleep(2000);
+			driver.findElement(By.id("idlist")).click();
+			sleep(5000);
+			om.cambiarVentanaNavegador(0);
+			sleep(12000);
+			om.completarFlujoOrquestacion();
+			
+			}
 
 }
