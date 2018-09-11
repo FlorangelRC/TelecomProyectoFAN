15daa1762f6b3cb1218737636fdf9612bcfd7635
angel leon <angel.numb@hotmail.com>
2018-09-11 09:54:20 -0300

    11/09/18

--- src/Tests/TestBase.java
+++ src/Tests/TestBase.java
@@ -14,6 +14,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -44,8 +45,8 @@ import DataProvider.ExcelUtils;
 
 public class TestBase {
 	protected static WebDriver driver;//
-	 public String urlAmbiente = "https://telecomcrm--uat.cs53.my.salesforce.com";
-	 //public String urlAmbiente = "https://crm--sit.cs14.my.salesforce.com/";
+	 //public String urlAmbiente = "https://telecomcrm--uat.cs53.my.salesforce.com";
+	 public String urlAmbiente = "https://crm--sit.cs14.my.salesforce.com/";
 	
 	
 	public void leftDropdown(WebDriver driver, String selection) {
@@ -1043,7 +1044,7 @@ public class TestBase {
 	@DataProvider
 	public Object[][] CuentaSuspension() throws Exception {
 		
-		Object[][] testObjArray = ExcelUtils.getTableArray("Cuentas.xlsx","PerfilGestiones",1,1,1,"Suspension");
+		Object[][] testObjArray = ExcelUtils.getTableArray("Cuentas.xlsx","PerfilGestiones",1,1,4,"Suspension");
 		
 		return(testObjArray);
 		
