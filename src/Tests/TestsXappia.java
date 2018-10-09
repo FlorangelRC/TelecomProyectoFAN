ad25e817547d3dba0143912ec47785175314270a
Nico <nicolas.fazzano@gmail.com>
2018-10-09 09:26:45 -0300

    Clase casos Xappia

--- src/Tests/TestsXappia.java
+++ src/Tests/TestsXappia.java
@@ -6,12 +6,16 @@ import org.testng.annotations.Test;
 
 import Pages.setConexion;
 
-public class TestsXappia extends TestBase{
+public class TestsXappia extends TestBase {
 
 	private WebDriver driver;
+	
+	public TestsXappia() {
+		driver = setConexion.setupEze();
+	}
 		
 	private void loginUAT() {
-		driver = setConexion.setupEze();
+		//driver = setConexion.setupEze();
 		driver.get("https://telecomcrm--uat.cs53.my.salesforce.com");
 		sleep(2000);
 		driver.findElement(By.id("idp_section_buttons")).click();
@@ -23,7 +27,7 @@ public class TestsXappia extends TestBase{
 	}
 	
 	private void loginSIT() {
-		driver = setConexion.setupEze();
+		//driver = setConexion.setupEze();
 		driver.get("https://crm--sit.cs14.my.salesforce.com/");
 		sleep(2000);
 		driver.findElement(By.id("idp_section_buttons")).click();
@@ -36,13 +40,13 @@ public class TestsXappia extends TestBase{
 	
 	
 	@Test
-	public void Gestiones_Del_Panel_Izquierdo_En_UAT() {
+	public void Gestiones_Del_Panel_Izquierdo_En_Consola_FAN_En_Ambiente_UAT() {
 		loginUAT();
  		
 	}
 	
 	@Test
-	public void SmokeTest_Tiempo_De_Carga_De_SIT() {
+	public void SmokeTest_Tiempo_De_Carga_De_Consola_FAN_En_Ambiente_SIT() {
 		loginSIT();
 		
 	}
