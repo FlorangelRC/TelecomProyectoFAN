b32cdf44087f26cb99b089342f99b28d41ccc13a
Nico <nicolas.fazzano@gmail.com>
2018-10-19 08:25:19 -0300

    Comunidad

--- src/Tests/Comunidad.java
+++ src/Tests/Comunidad.java
@@ -159,27 +159,17 @@ public class Comunidad extends TestBase {
 	}
 	
 	@Test (groups = {"Communities", "E2E"})
-	public void asd() throws AWTException {
-		mobileEmulation();
-		driver.switchTo().frame(cambioFrame(driver, By.cssSelector(".slds-size--1-of-1.slds-align-middle.slds-p-vertical--small.cursor")));
-		driver.findElement(By.cssSelector(".slds-size--1-of-1.slds-align-middle.slds-p-vertical--small.cursor")).click();
-		sleep(5000);
-		driver.switchTo().frame(cambioFrame(driver, By.cssSelector(".slds-size--1-of-1.slds-p-vertical--medium.slds-m-horizontal--xxx-small.slds-border--bottom.vloc-action-grid-cell.gestionlink")));
-		buscarYClick(driver.findElements(By.cssSelector(".slds-size--1-of-1.slds-p-vertical--medium.slds-m-horizontal--xxx-small.slds-border--bottom.vloc-action-grid-cell.gestionlink")), "contains", "mis servicios");
+	public void CRM_PRE_Community_Desktop_Alta_DNI_existente_en_Mi_cuenta_Cliente_existente_en_CRM() {
+		driver.get("https://sit-scrumcella.cs14.force.com/clientes/s/login/?startURL=%2Fclientes%2Fs%2F&ec=302");
 		sleep(3000);
-		driver.switchTo().frame(cambioFrame(driver, By.cssSelector(".slds-grid.slds-wrap.slds-card.slds-m-bottom--small.slds-p-around--medium")));
-		WebElement productos = null, servicios = null;
-		for (WebElement x : driver.findElements(By.cssSelector(".slds-grid.slds-wrap.slds-card.slds-m-bottom--small.slds-p-around--medium"))) {
-			if (x.getText().toLowerCase().contains("productos activos"))
-				productos = x;
-			if (x.getText().toLowerCase().contains("servicios incluidos"))
-				servicios = x;
-		}
-		WebElement tablaProductos = productos.findElement(By.className("slds-text-heading--label"));
-		WebElement tablaServicios = servicios.findElement(By.className("slds-text-heading--label"));
-		Assert.assertTrue(tablaProductos.getText().contains("NOMBRE") && tablaProductos.getText().contains("FECHA DE ACTIVACI\u00d3N") && tablaProductos.getText().contains("MONTO"));
-		Assert.assertTrue(tablaServicios.getText().contains("NOMBRE") && tablaServicios.getText().contains("FECHA DE ESTADO") && tablaServicios.getText().contains("ESTADO"));
-	}	
+		driver.findElement(By.cssSelector(".slds-button.slds-button--neutral.sfdc_button.register.uiButton--default.uiButton")).click();
+		sleep(3000);
+		driver.findElement(By.cssSelector(".input.sfdc_usernameinput.sfdc.input.uiInput.uiInputText.uiInput--default.uiInput--input")).sendKeys("22930856");
+		driver.findElement(By.cssSelector(".slds-button.slds-button--neutral.sfdc_button.checkBtn.uiButton--default.uiButton")).click();
+		sleep(2000);
+		String msj = driver.findElement(By.className("uiOutputRichText")).getText();
+		Assert.assertTrue(msj.contains(" ya tiene usuario, seleccione "));
+	}
 
 	@Test (groups = {"Communities","E2E"})
 	public void CRM_PRE_Community_Desktop_Gestiones_Abandonadas_Mayores_a_5(){
