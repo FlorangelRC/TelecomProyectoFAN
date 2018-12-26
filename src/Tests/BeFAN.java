958a378127cb9c8470a15afe9610264d3c9fde4a
Nico <nicolas.fazzano@gmail.com>
2018-12-26 15:47:03 -0300

    BeFAN

--- src/Tests/BeFAN.java
+++ src/Tests/BeFAN.java
@@ -1,8 +1,11 @@
 package Tests;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -29,8 +32,7 @@ public class BeFAN extends TestBase {
 						x.click();
 						sleep(3000);
 					}
-				}
-			
+				}			
 			} catch (Exception e) {}
 		case "gestion":
 			try {
@@ -40,26 +42,87 @@ public class BeFAN extends TestBase {
 						sleep(3000);
 					}
 				}
-			
 			} catch (Exception e) {}
 		}
 	}
 	
 
-	@BeforeClass
+	@BeforeClass (alwaysRun = true)
 	public void init() {
 		driver = setConexion.setupEze();
 		loginBeFAN(driver);
 	}
 	
-	@AfterMethod
+	//@AfterMethod (alwaysRun = true)
 	public void after() {
 		driver.get(TestBase.urlBeFAN);
 		sleep(3000);
 	}
 	
-	//@AfterClass
+	//@AfterClass (alwaysRun = true)
 	public void quit() {
 		driver.quit();
+	}
+	
+	@Test (groups = "BeFAN")
+	public void TS126656_BeFan_Movil_REPRO_Preactivacion_repro_Busqueda_de_archivos_Agente_Por_estado_EN_PROCESO_Exitosa() {
+		boolean enProceso = false;
+		irA("gestion");
+		selectByText(driver.findElement(By.cssSelector(".text.form-control.ng-pristine.ng-untouched.ng-valid.ng-empty")), "En Proceso");
+		selectByText(driver.findElements(By.cssSelector(".text.form-control.ng-pristine.ng-untouched.ng-valid.ng-empty")).get(1), "BAS-VJP-BAHIA BLANCA - VJP Punta Alta");
+		driver.findElement(By.cssSelector(".btn.btn-primary")).click();
+		sleep(5000);
+		List<WebElement> tabla = driver.findElement(By.id("exportarTabla")).findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+		for (int i=0; i<5; i++) {
+			if (tabla.iterator().next().findElements(By.tagName("td")).get(6).getText().equalsIgnoreCase("En Proceso"))
+				enProceso = true;
+			i++;
+		}
+		Assert.assertTrue(enProceso);
+	}
+	
+	@Test (groups = "BeFAN")
+	public void TS126655_BeFan_Movil_REPRO_Preactivacion_repro_Busqueda_de_archivos_Agente_Por_estado_PENDIENTE_Exitosa() {
+		boolean pendiente = false;
+		irA("gestion");
+		selectByText(driver.findElement(By.cssSelector(".text.form-control.ng-pristine.ng-untouched.ng-valid.ng-empty")), "Pendiente");
+		selectByText(driver.findElements(By.cssSelector(".text.form-control.ng-pristine.ng-untouched.ng-valid.ng-empty")).get(1), "BAS-VJP-BAHIA BLANCA - VJP Punta Alta");
+		driver.findElement(By.cssSelector(".btn.btn-primary")).click();
+		sleep(5000);
+		List<WebElement> tabla = driver.findElement(By.id("exportarTabla")).findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+		for (int i=0; i<5; i++) {
+			if (tabla.iterator().next().findElements(By.tagName("td")).get(6).getText().equalsIgnoreCase("Pendiente"))
+				pendiente = true;
+			i++;
+		}
+		Assert.assertTrue(pendiente);
+	}
+	
+	@Test (groups = "BeFAN")
+	public void TS126657_BeFan_Movil_REPRO_Preactivacion_repro_Busqueda_de_archivos_Agente_Por_estado_PROCESADO_Exitosa() {
+		boolean procesado = false;
+		irA("gestion");
+		selectByText(driver.findElement(By.cssSelector(".text.form-control.ng-pristine.ng-untouched.ng-valid.ng-empty")), "Procesado");
+		selectByText(driver.findElements(By.cssSelector(".text.form-control.ng-pristine.ng-untouched.ng-valid.ng-empty")).get(1), "BAS-VJP-BAHIA BLANCA - VJP Punta Alta");
+		driver.findElement(By.cssSelector(".btn.btn-primary")).click();
+		sleep(5000);
+		List<WebElement> tabla = driver.findElement(By.id("exportarTabla")).findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+		for (int i=0; i<5; i++) {
+			if (tabla.iterator().next().findElements(By.tagName("td")).get(6).getText().equalsIgnoreCase("Procesado"))
+				procesado = true;
+			i++;
+		}
+		Assert.assertTrue(procesado);
+	}
+	
+	@Test
+	public void TS126662_BeFan_Movil_REPRO_Preactivacion_repro_Busqueda_de_archivos_Agente_Visualizacion_de_mas_informacion() {
+		boolean procesado = false;
+		irA("gestion");
+		selectByText(driver.findElement(By.cssSelector(".text.form-control.ng-pristine.ng-untouched.ng-valid.ng-empty")), "Procesado");
+		selectByText(driver.findElements(By.cssSelector(".text.form-control.ng-pristine.ng-untouched.ng-valid.ng-empty")).get(1), "BAS-VJP-BAHIA BLANCA - VJP Punta Alta");
+		driver.findElement(By.cssSelector(".btn.btn-primary")).click();
+		sleep(5000);
+		
 	}
 }
